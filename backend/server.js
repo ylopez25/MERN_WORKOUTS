5e4850b5ff512192c7283bd9a4001d4f9ff07b66
Yesenia L <ylopezcr123@gmail.com>
2023-12-31 20:50:08 -0500

    working middleware

--- backend/server.js
+++ backend/server.js
@@ -2,18 +2,14 @@ const express = require('express');
 const app = express();
 require('dotenv').config()
 const PORT = process.env.PORT;
+const workoutsApi = require('./api/workouts')
 
-//middleware
-
+//middleweellleare
+app.use("/", workoutsApi)
 
 //routes
-app.get('/',(req,res) => {
-    res.json({mssg:'lets try this again'})
-})
 
-app.post('/',(req,res) => {
-   res.json({mssg:'post ROUTE'})
-})
+
 
 app.listen(PORT, () => {
     console.log(`Listenging on ${PORT}`)
