3283007ce52cc502a7ad70ae02d8fe4c4db4790f
Yesenia L <ylopezcr123@gmail.com>
2024-01-03 22:32:43 -0500

    new route

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 const PORT = process.env.PORT;
 const workoutsApi = require('./api/workouts')
 
-app.use("/", workoutsApi)
+app.use("/api/workouts", workoutsApi)
 
 app.listen(PORT, () => {
     console.log(`Listenging on ${PORT}`)
