7cb0b57cf9fe8a5ecd75b516e2681fee33dd79d1
Yesenia L <ylopezcr123@gmail.com>
2024-01-07 23:34:48 -0500

    install mangoose

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose')
 require('dotenv').config()
 const PORT = process.env.PORT;
 const workoutsApi = require('./api/workouts')
