03694ec3fc5ce78309b8e6e1ceff66b0628bfe69
Yesenia L <ylopezcr123@gmail.com>
2024-01-07 23:36:57 -0500

    connect to db

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose')
+const MONGO_URI= process.env.MONGO_URI;
 require('dotenv').config()
 const PORT = process.env.PORT;
 const workoutsApi = require('./api/workouts')
 
 app.use("/api/workouts", workoutsApi)
+
+//connect db 
+mongoose.connect(MONGO_URI)
 
 app.listen(PORT, () => {
     console.log(`Listenging on ${PORT}`)
