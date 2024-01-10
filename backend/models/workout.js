715afebcc52f597b8d14be6e01a54b1bf5c95cef
Yesenia L <ylopezcr123@gmail.com>
2024-01-09 21:35:01 -0500

    build schema

--- backend/models/workout.js
+++ backend/models/workout.js
@@ -1,3 +1,20 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-module.exports = mongoose
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    title : {
+        type: String.apply,
+        required: true
+    },
+    reps: {
+        type: Number,
+        required: true
+    },
+    load: {
+        type: Number,
+        required: true
+    }
+}, { timestamps: true })
+
+module.exports = mongoose.model('workout', workoutSchema)
