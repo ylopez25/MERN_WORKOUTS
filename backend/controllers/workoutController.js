db27bb7e6334f5d5dab913636584bf5da4af42e6
Yesenia L <ylopezcr123@gmail.com>
2024-01-10 15:06:42 -0500

    get all workouts

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -1,14 +1,31 @@
 const workout = require('../models/workoutModal')
 
-const postWorkout =  async (req,res) => {
-    const {load,reps,title} = req.body;
+//get all workouts
+const getAll = async (req,res) => {
+    const allWorkouts = await workout.find({}).sort({createdAt: -1})
     try {
-        const workoutObj = await workout.create({load,reps,title})
-        res.status(200).json({workoutObj})
+        res.status(200).json(allWorkouts)
     }catch (e) {
         res.status(400).json({e:e.message})
     }
 }
+
+//get one workout
+
+//post workout
+const postWorkout =  async (req,res) => {
+    const {load,reps,title} = req.body;
+    try {
+        const workoutObj = await workout.create({load,reps,title})
+        res.status(200).json(workoutObj)
+    }catch (e) {
+        res.status(400).json({e:e.message})
+    }
+}
+
+//delete workout
+
 module.exports= {
-    postWorkout
+    postWorkout,
+    getAll,
 }
