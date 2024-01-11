57e7c65df7e711aa2cb1c12b61789549a6a53c86
Yesenia L <ylopezcr123@gmail.com>
2024-01-11 16:03:22 -0500

    home page working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
       <Routes>
-        <Route>
-          
-        </Route>
+        <Route path='/' element={<Home/>} />
       </Routes>
       </BrowserRouter>
     </div>
