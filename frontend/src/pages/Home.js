f1a64d0475b8c3718a5e40c1fef009d77f8d20d6
Yesenia L <ylopezcr123@gmail.com>
2024-01-12 16:30:09 -0500

    workout usestate

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,25 @@
+import { useEffect, useState } from 'react'
 const Home = () => {
-return (
+    const [workouts, setWorkouts] = useState(null);
+
+    useEffect(()=>{
+        const fetchWorkouts = async () => {
+            const response = await fetch('https://localhost:3000/api/workouts')
+            const json = await response.json()
+
+            if(response.ok) {
+                setWorkouts(json)
+            }
+        }   
+        fetchWorkouts(json)
+     }, [])
+  return (
     <div>
-        <h1>
-            Hello world!
-        </h1>
+      <h2>Home</h2>
+      {workouts && workouts.map((workout)=> (
+        <p key={workout._id}>{workout.title}</p>
+      ))}
     </div>
-)
-}
-export default Home
+  );
+};
+export default Home;
