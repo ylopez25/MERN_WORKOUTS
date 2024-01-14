4337afd3b680bc53560447e68db2546806de9c10
Yesenia L <ylopezcr123@gmail.com>
2024-01-14 00:23:36 -0500

    removed cors error

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,24 +1,24 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
+
 const Home = () => {
-    const [workouts, setWorkouts] = useState(null);
+  const [workouts, setWorkouts] = useState(null);
 
-    useEffect(()=>{
-        const fetchWorkouts = async () => {
-            const response = await fetch('https://localhost:3000/api/workouts')
-            const json = await response.json()
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch("/api/workouts");
+      const json = await response.json();
 
-            if(response.ok) {
-                setWorkouts(json)
-            }
-        }   
-        fetchWorkouts()
-     }, [])
+      if (response.ok) {
+        setWorkouts(json);
+      }
+    };
+    fetchWorkouts();
+  }, []);
   return (
-    <div>
-      <h2>Home</h2>
-      {workouts && workouts.map((workout)=> (
-        <p key={workout._id}>{workout.title}</p>
-      ))}
+    <div className='home'>
+        <div className='workouts'>
+        {workouts && workouts.map((workout) =>( <p key={workout._id}>{workout.title}</p>))}
+        </div>
     </div>
   );
 };
