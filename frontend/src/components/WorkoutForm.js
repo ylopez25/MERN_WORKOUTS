4d2815a8d1cd0dec95ad17894d59ebe2c75a2bbe
Yesenia L <ylopezcr123@gmail.com>
2024-01-15 01:42:32 -0500

    form component

---
+++ frontend/src/components/WorkoutForm.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import { useState } from 'react';
+
+export default function WorkoutForm() {
+    const [title, setTitle] = useState('');
+    const [load, setLoad] = useState('');
+    const [reps, setReps] = useState('');
+    const [err, setError] = useState(null);
+
+    //handle submit
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const workout = {title, load, reps};
+
+        const response = await fetch('/api/workouts', {
+            method: 'POST',
+            body: JSON.Stringify(workout),
+            headers: {
+                'Content-Type' : 'application/json'
+            } 
+        }).json();
+
+        if(!response.ok) {
+            setError(response.err)
+        }
+
+        if(response.ok) {
+            setTitle('')
+            setLoad('')
+            setReps('')
+            setError(null)
+            console.log('new workout added', response)
+        }
+
+    }
+  return (
+    <form onSubmit={handleSubmit()}>
+        <label>Title:</label>
+        <input type="text"
+        onChange= {(e) => setTitle(e.target.value)}
+        value={title}
+        />
+
+        <label>Load (kg):</label>
+        <input type="text" 
+        onChange = {(e) => setLoad(e.target.value)}
+        value={load}
+        />
+
+        <label>Reps:</label>
+        <input type="text"
+        onChange = {(e) => setReps(e.target.value)}
+        value={reps}
+        />
+
+        <button>Submit</button>
+    </form>
+  )
+}
