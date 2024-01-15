4127193a02419a65f2b394048fcbfd138d233be9
Yesenia L <ylopezcr123@gmail.com>
2024-01-14 22:32:13 -0500

    css for homepage

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
-export default function workoutDetails({workout}) {
+export default function workoutDetails({ workout }) {
   return (
-    <div>
+    <div className='workout-details'>
         <h2>{workout.title}</h2>
         <p>Load kg: {workout.load}</p>
         <p>Reps: {workout.reps}</p>
+        <p>
+            {workout.createdAt}
+        </p>
     </div>
   )
 }
