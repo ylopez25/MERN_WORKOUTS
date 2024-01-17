31449fe975101d2446e5c05a74f692930b1163cd
Yesenia L <ylopezcr123@gmail.com>
2024-01-17 16:53:59 -0500

    working delete button

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,12 +1,26 @@
 import React from "react";
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
 
-export default function workoutDetails({ workout }) {
+export default function WorkoutDetails({ workout }) {
+  const {dispatch} = useWorkoutsContext()
+
+  const handleDelete = async () => {
+    const response = await fetch('/api/workouts/' + workout._id, {
+      method: 'DELETE'
+    })
+    const json = await response.json();
+
+    if(response.ok) {
+      dispatch({ type:'DELETE_WORKOUT', payload: json})
+    }
+  }
   return (
     <div className="workout-details">
       <h2>{workout.title}</h2>
       <p>Load kg: {workout.load}</p>
       <p>Reps: {workout.reps}</p>
       <p>{workout.createdAt}</p>
+      <span onClick={handleDelete}>Delete</span>
     </div>
   );
 }
