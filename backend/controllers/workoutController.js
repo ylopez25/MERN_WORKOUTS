028b68673a94d2e4b6f928a2549e67fc5ce45829
Yesenia L <ylopezcr123@gmail.com>
2024-01-18 23:32:45 -0500

    error messages on empty fields

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -27,6 +27,22 @@ const getOneWorkout = async (req,res) => {
 //post workout
 const postWorkout =  async (req,res) => {
     const {title,reps,load} = req.body;
+
+    //error message for empty fields
+    let emptyFields = [];
+
+    if(!title) {
+        emptyFields.push('title')
+    }
+    if(!load) {
+        emptyFields.push('load')
+    }
+    if(!reps) {
+        emptyFields.push('reps')
+    }
+    if(emptyFields.length > 0) {
+       return res.status(400).json({error: 'Please fill out all fields', emptyFields})
+    }
     try {
         const workoutObj = await workout.create({title,reps,load})
         res.status(200).json(workoutObj)
