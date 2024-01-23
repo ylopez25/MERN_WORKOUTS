bebcc03af8f7db58cb8cc82d22866648e3742e1c
Yesenia L <ylopezcr123@gmail.com>
2024-01-22 23:54:04 -0500

    user route setup

---
+++ backend/routes/user.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const route = express.Router();
+
+//login
+router.post('/login', () => {
+
+})
+//signup
+router.post('/signup', () => {
+
+})
+
+module.exports=router;
