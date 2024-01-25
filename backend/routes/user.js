af723f7ad64b1bf4b142bed6f970aa0626c21644
Yesenia L <ylopezcr123@gmail.com>
2024-01-24 23:31:09 -0500

    res and req

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,12 +2,12 @@ const express = require('express');
 const route = express.Router();
 
 //login
-route.post('/login', () => {
-
+route.post('/login', (res,req) => {
+    res.status(200).json({message: 'WORKING login'})
 })
 //signup
-route.post('/signup', () => {
-
+route.post('/signup', (req,res) => {
+    res.status(200).json({message:'WORKING signup'})
 })
 
 module.exports=route;
