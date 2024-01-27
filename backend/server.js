69bcf789fb351072a93baf2200083ee6d20e8fd2
Yesenia L <ylopezcr123@gmail.com>
2024-01-27 15:39:01 -0500

    connect api with server.js

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,14 @@ const mongoose = require('mongoose');
 require('dotenv').config()
 const PORT = process.env.PORT;
 const workoutsApi = require('./routes/workouts')
+const userApi = require('./routes/user')
 const bodyParser = require('body-parser');
 //bodyparser
 app.use(bodyParser.json())
+app.use(nodyParser.json())
+
 app.use("/api/workouts", workoutsApi)
+app.use("/api/users", userApi)
 
 //connect db 
 mongoose.connect(process.env.MONGO_URI)
